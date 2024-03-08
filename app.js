2867429c78332cb5d33663a3f02d9642affc6d34
Cauan Diniz <cauanrodiniz@gmail.com>
2024-03-08 20:01:51 -0300

    feat: routes

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const checklistRouter = require('./src/routes/checklist')
+const app = express()
+
+app.use(express.json())
+app.use('/checklists', checklistRouter)
+
+app.listen(3000, () => {
+    console.log("Server has been started.");
+})
